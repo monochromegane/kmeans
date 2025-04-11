f64e7d54210271c18330f33358d36bfaa6787161
monochromegane <dev.kuro.obi@gmail.com>
2025-04-11 17:45:00 +0900

    Update error definitions.

--- errors.go
+++ errors.go
@@ -1,21 +1,15 @@
 package kmeans
 
-import "fmt"
+import "errors"
 
-var ErrInvalidInitMethod = fmt.Errorf("invalid init method")
-
-var ErrInvalidDataLength = fmt.Errorf("data length must be divisible by the number of features")
-
-var ErrInvalidNumClusters = fmt.Errorf("number of clusters must be greater than 0")
-
-var ErrInvalidNumFeatures = fmt.Errorf("number of features must be greater than 0")
-
-var ErrEmptyData = fmt.Errorf("data is empty")
-
-var ErrFewerClustersThanData = fmt.Errorf("number of clusters must be less than the number of data points")
-
-var ErrInvalidMaxIterations = fmt.Errorf("max iterations must be greater than 0")
-
-var ErrInvalidTolerance = fmt.Errorf("tolerance must be greater than 0")
-
-var ErrInvalidConcurrency = fmt.Errorf("concurrency must be greater than 0")
+var (
+	ErrInvalidInitMethod     = errors.New("invalid init method")
+	ErrInvalidDataLength     = errors.New("data length must be divisible by the number of features")
+	ErrInvalidNumClusters    = errors.New("number of clusters must be greater than 0")
+	ErrInvalidNumFeatures    = errors.New("number of features must be greater than 0")
+	ErrEmptyData             = errors.New("data is empty")
+	ErrFewerClustersThanData = errors.New("number of clusters must be less than the number of data points")
+	ErrInvalidMaxIterations  = errors.New("max iterations must be greater than 0")
+	ErrInvalidTolerance      = errors.New("tolerance must be greater than 0")
+	ErrInvalidConcurrency    = errors.New("concurrency must be greater than 0")
+)
